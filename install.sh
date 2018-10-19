d8b3afbda7c46fe0ca5484452bc923851fdaa520
Théophile Jacob <44201921+Ethyrr@users.noreply.github.com>
2018-10-19 19:33:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,8 +173,6 @@ fi
 echo; echo 'Installation has completed!'
 echo 'Config files are located at /etc/ddos/'
 echo
-echo 'Please send in your comments and/or suggestions to:'
-echo 'https://github.com/jgmdev/ddos-deflate/issues'
 echo
 
 exit 0
